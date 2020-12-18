e9297e64930d92052b356dc68e38e62088024a8c
Manuel Salvatore Martone <manuel.martone@gmail.com>
2020-12-18 20:00:26 +0100

    datastore moved to postgres

--- bin/common/env-values.js
+++ bin/common/env-values.js
@@ -1,3 +1,4 @@
 module.exports = {
-  mysqlDatabaseUrl: process.env.MYSQL_DATABASE_URL || process.env.JAWSDB_URL || process.env.CLEARDB_DATABASE_URL
+  mysqlDatabaseUrl: process.env.MYSQL_DATABASE_URL || process.env.JAWSDB_URL || process.env.CLEARDB_DATABASE_URL,
+  postgreDatabaseUrl: process.env.POSTGRES_DATABASE_URL || process.env.POSTGRES_URL
 }
