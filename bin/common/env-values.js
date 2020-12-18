e33f08910b0c9dadcff0cb48b4ad992b5f4b7268
Manuel Salvatore Martone <manuel.martone@gmail.com>
2020-12-18 20:20:36 +0100

    fixed wait-for-db issue with error var name updated to correct config var for postgres DATABASE_URL

--- bin/common/env-values.js
+++ bin/common/env-values.js
@@ -1,4 +1,4 @@
 module.exports = {
   mysqlDatabaseUrl: process.env.MYSQL_DATABASE_URL || process.env.JAWSDB_URL || process.env.CLEARDB_DATABASE_URL,
-  postgreDatabaseUrl: process.env.POSTGRES_DATABASE_URL || process.env.POSTGRES_URL
+  postgreDatabaseUrl: process.env.DATABASE_URL
 }
